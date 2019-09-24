04527737a1bed0e93c97655ef0f58ccb668293ec
Nicolas Pastor <nicolas.pastor@globant.com>
2019-09-24 15:46:13 -0300

    otro commit

--- src/main/java/com/nico/demoasinc/DemoAsincApplication.java
+++ src/main/java/com/nico/demoasinc/DemoAsincApplication.java
@@ -1,13 +1,85 @@
 package com.nico.demoasinc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.sun.org.glassfish.external.statistics.impl.StatisticImpl.START_TIME;
 
 @SpringBootApplication
-public class DemoAsincApplication {
+@EnableAsync
+public class DemoAsincApplication implements CommandLineRunner {
+
+	@Autowired
+	ServiceImpl servicio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAsincApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		LocalTime inicio = LocalTime.now();
+
+		List<Integer> integerList = new ArrayList<>();
+		integerList.add(5000);
+		integerList.add(5000);
+		integerList.add(11000);
+
+		System.out.println("inicio");
+		List<CompletableFuture> completableFutureList = integerList
+				.stream()
+				.map( x -> servicio.voidMethodString(x)).collect(Collectors.toList());
+
+
+		System.out.println("antes allof");
+		CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[completableFutureList.size()]));
+
+
+		System.out.println("antes join");
+		//CompletableFuture<List<Object>> listCompletableFuture = voidCompletableFuture.thenApply(x -> {
+		//	return completableFutureList.stream().map(y -> y.join()).collect(Collectors.toList());
+		//});
+
+
+		System.out.println("antes obtencion");
+		List<Object> collect = voidCompletableFuture.thenApply(x -> {
+			return completableFutureList.stream().map(y -> y.join()).collect(Collectors.toList());
+		}).get();
+
+
+		//System.out.println("antes obtencion");
+		//List<Object> collect = listCompletableFuture.get().stream().collect(Collectors.toList());
+
+
+		System.out.println("lista:" + collect);
+
+
+		long serviceTime = ChronoUnit.MILLIS.between( inicio, LocalTime.now());
+
+		System.out.println("termino:" + serviceTime);
+		//allof.get();
+
+		//if (allof.isDone ()) {
+			//System.out.println("Future result " + stringCompletableFuture.get() + " | " + stringCompletableFuture2.get() + " | " + stringCompletableFuture3.get());
+		//} else {
+		//	System.out.println("Futures are not ready");
+		//}
+
+
+
+
+
+	}
 }
---
+++ src/main/java/com/nico/demoasinc/ServiceImpl.java
@@ -0,0 +1,53 @@
+package com.nico.demoasinc;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+@Service
+public class ServiceImpl {
+
+    @Async
+    public CompletableFuture<String> voidMethodString(int aaa) {
+        try {
+            Thread.sleep(aaa);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("MederuQ: " + aaa);
+        return CompletableFuture.completedFuture("MederuQ: " + aaa);
+    }
+
+
+    @Async
+    public CompletableFuture<Integer> voidMethodInteger(int aaa) {
+        try {
+            Thread.sleep(aaa);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("MederuQ: " + aaa);
+        return CompletableFuture.completedFuture(new Integer(102030));
+    }
+
+
+    @Async
+    public CompletableFuture<Double> voidMethodDouble(int aaa) {
+        try {
+            Thread.sleep(aaa);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("MederuQ: " + aaa);
+        return CompletableFuture.completedFuture(new Double(203040.11));
+    }
+
+    @Async
+    public String otroMetodo(int asd){
+        return "Hola: " + asd;
+
+    }
+}
