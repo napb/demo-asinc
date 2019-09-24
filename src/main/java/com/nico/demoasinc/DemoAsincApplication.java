9cc931aa1a775454c621736ae92e9f60bae29fe1
Nicolas Pastor <nicolas.pastor@globant.com>
2019-09-24 11:47:47 -0300

    Initial commit

---
+++ src/main/java/com/nico/demoasinc/DemoAsincApplication.java
@@ -0,0 +1,13 @@
+package com.nico.demoasinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAsincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAsincApplication.class, args);
+	}
+
+}
